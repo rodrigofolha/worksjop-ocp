95774ebc782e46c16ca60dc2091dc032e1171acd
rodrigofolha <vinigo2@gmail.com>
2018-05-15 16:14:20 -0300

    Update jenkins-pipeline.groovy

--- jenkins-pipeline.groovy
+++ jenkins-pipeline.groovy
@@ -1,14 +1,14 @@
  node('maven') {
     stage('build') {
         echo 'building app :)'
-        openshiftBuild(buildConfig: 'workshop-redhat', showBuildLogs: 'true')
+        openshiftBuild(buildConfig: 'workshop-ocp', showBuildLogs: 'true')
     }
     stage('verify') {
         echo 'dummy verification....'
     }
     stage('deploy') {
         input 'Manual Approval'
-        openshiftDeploy(deploymentConfig: 'workshop-redhat')
+        openshiftDeploy(deploymentConfig: 'workshop-ocp')
     }
     stage('promoting to QA') {
        echo 'fake stage...'
